e52fdd463ed0e208d8f8c383187a53fe6d3e959f
Jason Sooter <jason.sooter@algolia.com>
2019-01-12 00:06:09 -0600

    feat(lessons): add lesson 2

---
+++ testing-fundamentals/lesson-2.js
@@ -0,0 +1,16 @@
+const { sum, subtract } = require('./math');
+
+expect(sum(3, 7)).toBe(10);
+expect(subtract(7, 3)).toBe(4);
+
+function expect(actual) {
+  return {
+    toBe(expected) {
+      if (actual !== expected) {
+        throw new Error(`${actual} is not equal to ${expected}`);
+      }
+    },
+    // toEqual(expected) {},
+    // toBeGreaterThan(expected) {},
+  };
+}
